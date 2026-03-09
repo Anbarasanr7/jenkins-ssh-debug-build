eae6b39cfddd7971a403c2c383a4c2ae6d9527a9
Anbarasanr7 <30725046+Anbarasanr7@users.noreply.github.com>
2026-03-09 16:42:57 +0530

    Update start_direct_sshd_debug.sh

--- scripts/start_direct_sshd_debug.sh
+++ scripts/start_direct_sshd_debug.sh
@@ -2,21 +2,7 @@
 
 echo "Starting SSH server..."
 
-useradd -m debuguser
-mkdir -p /home/debuguser/.ssh
+service ssh start
 
-echo "$DEBUG_PUBLIC_KEY" > /home/debuguser/.ssh/authorized_keys
-
-chmod 600 /home/debuguser/.ssh/authorized_keys
-chown -R debuguser:debuguser /home/debuguser/.ssh
-
-/usr/sbin/sshd
-
-IP=$(hostname -i)
-
-echo "=================================="
-echo "SSH into container using:"
-echo "ssh debuguser@$IP"
-echo "=================================="
-
-sleep 600
+echo "Container SSH ready"
+echo "Connect using container IP"
